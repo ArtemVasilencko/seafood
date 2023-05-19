a12a05d4fe22e22f3e1f86a25722bd8fbeb434de
freddypopa <gamersuper801@gmail.com>
2023-05-19 10:46:37 +0300

    ended header

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+  $('.header-slider').slick({
+    vertical: true,
+    dots: true,
+    verticalSwiping: true,
+    prevArrow: '<button type="button" class="slick-prev" alt="" > <img src="img/prev-arrow.svg"</button>',
+    nextArrow: '<button type="button" class="slick-next"  alt="" ><img src="img/next-arrow.svg"</button>'
+  });
+});
