1b2854d33252cd7e16959743b372dd838ed12c7d
freddypopa <gamersuper801@gmail.com>
2023-05-22 13:52:27 +0300

    popa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 $(function () {
 
-  $('.header-slider').slick({
+  // $('.header-slider').slick({
+  //   vertical: true,
+  //   dots: true,
+  //   verticalSwiping: true,
+  //   prevArrow: '<button type="button" class="slick-prev" alt="" > <img src="img/prev-arrow.svg"</button>',
+  //   nextArrow: '<button type="button" class="slick-next"  alt="" ><img src="img/next-arrow.svg"</button>'
+  // });
+
+  $('.product__name').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.product__content',
+    focusOnSelect: true,
     vertical: true,
-    dots: true,
     verticalSwiping: true,
-    prevArrow: '<button type="button" class="slick-prev" alt="" > <img src="img/prev-arrow.svg"</button>',
-    nextArrow: '<button type="button" class="slick-next"  alt="" ><img src="img/next-arrow.svg"</button>'
-  });
+  })
+  $('.product__content').slick({
+    asNavFor: '.product__name',
+    slidesToShow: 1,
+    vertical: true,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+  })
+
 });
